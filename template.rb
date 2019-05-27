0636b68c381568be4014e97749be4ef5b9cc6848
Joseph Hyatt <josephnhyatt@gmail.com>
2019-05-27 14:56:12 -0700

    updated rails template

--- template.rb
+++ template.rb
@@ -1,5 +1,5 @@
-require "fileutils"
-require "shellwords"
+require "fileutils" # Several file utility methods for copying, moving, removing, etc
+require "shellwords" # Ruby standard library shellwords
 
 # Add this template directory to source_paths so that Thor actions like
 # copy_file and template resolve against our source files. If this file was
@@ -24,33 +24,51 @@ def add_template_repository_to_source_path
   end
 end
 
+def rails_version
+  @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
+end
+
+def rails_5?
+  Gem::Requirement.new(">= 5.2.0", "< 6.0.0.beta1").satisfied_by? rails_version
+end
+
+def rails_6?
+  Gem::Requirement.new(">= 6.0.0.beta1", "< 7").satisfied_by? rails_version
+end
+
 def add_gems
-  gem 'administrate', '~> 0.10.0'
-  gem 'bootstrap', '~> 4.1', '>= 4.1.1'
-  gem 'data-confirm-modal', '~> 1.6', '>= 1.6.2'
-  gem 'devise', '~> 4.4', '>= 4.4.3'
+  gem 'administrate', '~> 0.11.0' # Provides a better user experience for site admins, and to be easier for developers to customize
+  gem 'awesome_print', '~> 1.8' # Great Ruby dubugging companion: pretty print Ruby objects to visualize their structure
+  gem 'better_errors', '~> 2.5', '>= 2.5.1' # Provides a better error page for Rails and other Rack apps
+  gem 'bootstrap', '~> 4.3', '>= 4.3.1' # The most popular HTML, CSS, and JavaScript framework for developing responsive, mobile first projects on the web
+  gem 'devise', '~> 4.6', '>= 4.6.2' # Flexible authentication solution for Rails with Warden
   gem 'devise-bootstrapped', github: 'excid3/devise-bootstrapped', branch: 'bootstrap4'
-  gem 'devise_masquerade', '~> 0.6.2'
-  gem 'font-awesome-sass', '~> 5.5', '>= 5.5.0.1'
-  gem 'foreman', '~> 0.84.0'
-  gem 'friendly_id', '~> 5.2', '>= 5.2.4'
+  gem 'devise_masquerade', '~> 0.6.2' # Devise masquerade library
+  gem 'font-awesome-sass', '~> 5.8', '>= 5.8.1' # Font-Awesome SASS gem for use in Ruby projects
+  gem 'friendly_id', '~> 5.2', '>= 5.2.5' # Lets you create pretty URLs and work with human-friendly strings as if they were numeric ids
   gem 'gravatar_image_tag', github: 'mdeering/gravatar_image_tag'
-  gem 'jquery-rails', '~> 4.3.1'
-  gem 'local_time', '~> 2.0', '>= 2.0.1'
-  gem 'mini_magick', '~> 4.8'
-  gem 'name_of_person', '~> 1.0'
-  gem 'omniauth-facebook', '~> 5.0'
-  gem 'omniauth-github', '~> 1.3'
-  gem 'omniauth-twitter', '~> 1.4'
-  gem 'sidekiq', '~> 5.1', '>= 5.1.3'
-  gem 'sitemap_generator', '~> 6.0', '>= 6.0.1'
-  gem 'webpacker', '~> 3.5', '>= 3.5.3'
-  gem 'whenever', require: false
+  gem 'mini_magick', '~> 4.9', '>= 4.9.3' # Manipulate images with minimal use of memory via ImageMagick / GraphicsMagick
+  gem 'name_of_person', '~> 1.1', '>= 1.1.1' # Presenting names of people in full, familiar, abbreviated, and initialized forms (but without titulation etc)
+  gem 'omniauth-facebook', '~> 5.0' # Facebook OAuth2 Strategy for OmniAuth
+  gem 'omniauth-github', '~> 1.3' # Official OmniAuth strategy for GitHub
+  gem 'omniauth-twitter', '~> 1.4' # OmniAuth strategy for Twitter
+  gem 'sidekiq', '~> 5.2', '>= 5.2.7' # Simple, efficient background processing for Ruby
+  gem 'sitemap_generator', '~> 6.0', '>= 6.0.2' # Video, News, Image, Mobile, PageMap and Alternate Links sitemap extensions and includes Rake tasks for managing your sitemaps
+  gem 'whenever', require: false # Clean ruby syntax for writing and deploying cron jobs
+
+  if rails_5?
+    gsub_file "Gemfile", /gem 'sqlite3'/, "gem 'sqlite3', '~> 1.3.0'"
+    gem 'webpacker', '~> 4.0.1'
+  end
 end
 
 def set_application_name
   # Add Application Name to Config
-  environment "config.application_name = Rails.application.class.parent_name"
+  if rails_5?
+    environment "config.application_name = Rails.application.class.parent_name"
+  else
+    environment "config.application_name = Rails.application.class.module_parent_name"
+  end
 
   # Announce the user where he can change the application name in the future.
   puts "You can change application name inside: ./config/application.rb"
@@ -81,10 +99,7 @@ def add_users
     gsub_file migration, /:admin/, ":admin, default: false"
   end
 
-  requirement = Gem::Requirement.new("> 5.2")
-  rails_version = Gem::Version.new(Rails::VERSION::STRING)
-
-  if requirement.satisfied_by? rails_version
+  if Gem::Requirement.new("> 5.2").satisfied_by? rails_version
     gsub_file "config/initializers/devise.rb",
       /  # config.secret_key = .+/,
       "  config.secret_key = Rails.application.credentials.secret_key_base"
@@ -94,29 +109,47 @@ def add_users
   inject_into_file("app/models/user.rb", "omniauthable, :masqueradable, :", after: "devise :")
 end
 
-def add_bootstrap
-  # Remove Application CSS
-  run "rm app/assets/stylesheets/application.css"
+def add_webpack
+  # Rails 6+ comes with webpacker by default, so we can skip this step
+  return if rails_6?
 
-  # Add Bootstrap JS
-  insert_into_file(
-    "app/assets/javascripts/application.js",
-    "\n//= require jquery\n//= require popper\n//= require bootstrap\n//= require data-confirm-modal\n//= require local-time",
-    after: "//= require rails-ujs"
-  )
+  # Our application layout already includes the javascript_pack_tag,
+  # so we don't need to inject it
+  rails_command 'webpacker:install'
+end
+
+def add_javascript
+  run "yarn add expose-loader jquery popper.js bootstrap data-confirm-modal local-time"
+
+  if rails_5?
+    run "yarn add turbolinks @rails/actioncable@pre @rails/actiontext@pre @rails/activestorage@pre @rails/ujs@pre"
+  end
+
+  content = <<-JS
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Rails: '@rails/ujs'
+}))
+  JS
+
+  insert_into_file 'config/webpack/environment.js', content + "\n", before: "module.exports = environment"
 end
 
 def copy_templates
+  remove_file "app/assets/stylesheets/application.css"
+
+  copy_file "Procfile"
+  copy_file "Procfile.dev"
+  copy_file ".foreman"
+
   directory "app", force: true
   directory "config", force: true
   directory "lib", force: true
 
   route "get '/terms', to: 'home#terms'"
   route "get '/privacy', to: 'home#privacy'"
-end
-
-def add_webpack
-  rails_command 'webpacker:install'
 end
 
 def add_sidekiq
@@ -126,13 +159,12 @@ def add_sidekiq
     "require 'sidekiq/web'\n\n",
     before: "Rails.application.routes.draw do"
 
-  insert_into_file "config/routes.rb",
-    "  authenticate :user, lambda { |u| u.admin? } do\n    mount Sidekiq::Web => '/sidekiq'\n  end\n\n",
-    after: "Rails.application.routes.draw do\n"
-end
-
-def add_foreman
-  copy_file "Procfile"
+  content = <<-RUBY
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+  RUBY
+  insert_into_file "config/routes.rb", "#{content}\n\n", after: "Rails.application.routes.draw do\n"
 end
 
 def add_announcements
@@ -141,16 +173,16 @@ def add_announcements
 end
 
 def add_notifications
-  generate "model Notification recipient_id:integer actor_id:integer read_at:datetime action:string notifiable_id:integer notifiable_type:string"
+  generate "model Notification recipient_id:bigint actor_id:bigint read_at:datetime action:string notifiable_id:bigint notifiable_type:string"
   route "resources :notifications, only: [:index]"
 end
 
 def add_administrate
   generate "administrate:install"
 
-  gsub_file "app/dashboards/announcement_dashboard.rb",
-    /announcement_type: Field::String/,
-    "announcement_type: Field::Select.with_options(collection: Announcement::TYPES)"
+  #gsub_file "app/dashboards/announcement_dashboard.rb",
+    #/announcement_type: Field::String/,
+    #"announcement_type: Field::Select.with_options(collection: Announcement::TYPES)"
 
   gsub_file "app/dashboards/user_dashboard.rb",
     /email: Field::String/,
@@ -163,9 +195,7 @@ def add_administrate
   gsub_file "app/controllers/admin/application_controller.rb",
     /# TODO Add authentication logic here\./,
     "redirect_to '/', alert: 'Not authorized.' unless user_signed_in? && current_user.admin?"
-end
 
-def add_app_helpers_to_administrate
   environment do <<-RUBY
     # Expose our application's helpers to Administrate
     config.to_prepare do
@@ -183,15 +213,12 @@ def add_multiple_authentication
     generate "model Service user:references provider uid access_token access_token_secret refresh_token expires_at:datetime auth:text"
 
     template = """
-  if Rails.application.secrets.facebook_app_id.present? && Rails.application.secrets.facebook_app_secret.present?
-    config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'email,user_posts'
-  end
-  if Rails.application.secrets.twitter_app_id.present? && Rails.application.secrets.twitter_app_secret.present?
-    config.omniauth :twitter, Rails.application.secrets.twitter_app_id, Rails.application.secrets.twitter_app_secret
-  end
-  if Rails.application.secrets.github_app_id.present? && Rails.application.secrets.github_app_secret.present?
-    config.omniauth :github, Rails.application.secrets.github_app_id, Rails.application.secrets.github_app_secret
-  end
+    env_creds = Rails.application.credentials[Rails.env.to_sym] || {}
+    %i{ facebook twitter github }.each do |provider|
+      if options = env_creds[provider]
+        config.omniauth provider, options[:app_id], options[:app_secret], options.fetch(:options, {})
+      end
+    end
     """.strip
 
     insert_into_file "config/initializers/devise.rb", "  " + template + "\n\n",
@@ -229,33 +256,35 @@ after_bundle do
   set_application_name
   stop_spring
   add_users
-  add_bootstrap
-  add_sidekiq
-  add_foreman
   add_webpack
+  add_javascript
   add_announcements
   add_notifications
   add_multiple_authentication
+  add_sidekiq
   add_friendly_id
 
   copy_templates
+  add_whenever
+  add_sitemap
 
   # Migrate
-  rails_command "db:drop"
   rails_command "db:create"
   rails_command "db:migrate"
 
   # Migrations must be done before this
   add_administrate
 
-  add_app_helpers_to_administrate
-
-  add_whenever
-
-  add_sitemap
-
-
+  # Commit everything to git
   git :init
   git add: "."
   git commit: %Q{ -m 'Initial commit' }
+
+  say
+  say "Rails app successfully created!", :blue
+  say
+  say "To get started with your new app:", :green
+  say "cd #{app_name} - Switch to your new app's directory."
+  say "foreman start - Run Rails, sidekiq, and webpack-dev-server."
+  say "on server: http://localhost:5000/"
 end
